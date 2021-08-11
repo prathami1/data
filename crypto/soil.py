719683c14c6b8bf0f4b15d2caa59321dcd5242e8
Pratham <62070812+prathami1@users.noreply.github.com>
2021-08-10 22:27:39 -0400

    soil update

---
+++ crypto/soil.py
@@ -0,0 +1,107 @@
+<div id="about" class="min100vh container-fluid">
+                <div class="row align-self-top w-100 align-items-start justify-content-center">
+                    <div class="pt-0 offseted col-sm-9 col-md-9 offset-sm-2 col-10 offset-1">
+                        <div class="timeline alt"></div> <small class="section-title bullet-title alt">About /&gt;</small>
+                        <div class="mt-3">
+                            <div class="code-wrp">
+                                <div data-line-nr="01" class="code-l">
+                                    <div class="methods">class</div> <span class="propname">Pratham Inamdar</span> {
+                                </div>
+                                <div data-line-nr="02" class="code-l">
+                                    <div class="dot">··</div>
+                                    <div class="comment">// I'm ambitious and love new challenges </div>
+                                </div>
+                                <div data-line-nr="03" class="code-l">
+                                    <div class="dot">··</div>
+                                    <div class="comment">// My vast variety of skills is continuously expanding.</div>
+                                </div>
+                                <div data-line-nr="04" class="code-l">
+                                    <div class="dot">··</div>
+                                    <div class="methods">constructor</div>() {
+                                </div>
+                                <div data-line-nr="05" class="code-l">
+                                    <div class="dot">····</div><span class="scope">this</span>.<span class="prop2">name</span> <span class="scope">=</span> <span class="string">'Pratham Inamdar'</span>
+                                </div>
+                                <div data-line-nr="06" class="code-l">
+                                    <div class="dot">····</div><span class="scope">this</span>.<span class="prop2">dayOfBirthTimestamp</span> <span class="scope">=</span> <span class="number">10052004</span>
+                                </div>
+                                <!--<div data-line-nr="07" class="code-l">
+                      <div class="dot">····</div><span class="scope">this</span>.<span class="prop2">email</span> <span
+                        class="scope">=</span> <span class="string">'baszczok@yasio.pl'</span>
+                    </div>-->
+                                <div data-line-nr="08" class="code-l">
+                                    <div class="dot">··</div>}
+                                </div>
+                                <div data-line-nr="09" class="code-l">
+                                    <div class="dot">··</div><span class="propname">workExperience</span>() {
+                                </div>
+                                <div data-line-nr="10" class="code-l">
+                                    <div class="dot">····</div>
+                                    <div class="methods">return</div> [
+                                </div>
+                                <div data-line-nr="11" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2018-2019'</span> :<span class="string">
+                        'Head Manager Volunteer at Aloha Mind Math'</span> }
+                                </div>
+                                <div data-line-nr="12" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2019-Present'</span>:<span class="string">
+                        'DP Comittee Board Member'</span> }
+                                </div>
+                                <div data-line-nr="13" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2018-2020'</span>:<span class="string">
+                        'NBTID Board Volunteer'</span> }
+                                </div>
+                                <div data-line-nr="14" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2020-Present'</span> :<span class="string">
+                        'Full-Stack Developer at prathami1.studio'</span> }
+                                </div>
+                                <div data-line-nr="15" class="code-l">
+                                    <div class="dot">····</div>]
+                                </div>
+                                <div data-line-nr="16" class="code-l">
+                                    <div class="dot">··</div>}
+                                </div>
+                                <div data-line-nr="17" class="code-l">
+                                    <div class="dot">··</div><span class="propname">education</span>() {
+                                </div>
+                                <div data-line-nr="18" class="code-l">
+                                    <div class="dot">····</div>
+                                    <div class="methods">return</div> [
+                                </div>
+                                <div data-line-nr="19" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2018-2022'</span>:<span class="string">
+                        'South Brunswick High School Class of 2022'</span> },
+                                </div>
+                                <div data-line-nr="20" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2020'</span>:<span class="string">
+                        'Harvard Course of Data Analysis - Coursera'</span> },
+                                </div>
+                                <div data-line-nr="21" class="code-l">
+                                    <div class="dot">······</div>{ <span class="string">'2020'</span>:<span class="string">
+                        'Harvard Course of Quantum Analysis - Coursera'</span> }
+                                </div>
+                                <div data-line-nr="22" class="code-l">
+                                    <div class="dot">····</div>]
+                                </div>
+                                <div data-line-nr="23" class="code-l">
+                                    <div class="dot">··</div>}
+                                </div>
+                                <div data-line-nr="24" class="code-l">
+                                    <div class="dot">··</div><span class="propname">skills</span>() {
+                                </div>
+                                <div data-line-nr="25" class="code-l">
+                                    <div class="dot">····</div>
+                                    <div class="methods">return</div> [ <span class="string">'HTML5.1/CSS4/JS', 'Vue.js', 'jQuery',
+                        'Node.js', 'Bootstrap/Bulma/Material Design', 'JavaScript',
+                        'Electron', 'Web-extensions', 'Okapi Library', 'Firebase', 'PROS', 
+                        'Wordpress', 'UX/UI', 'C++', 'Java', 'Python', 'Selenium', </span> <span class="cursor"></span> ]
+                                </div>
+                                <div data-line-nr="26" class="code-l">
+                                    <div class="dot">··</div>}
+                                </div>
+                                <div data-line-nr="27" class="code-l">}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <a href="#contact" aria-label="Go to next" class="cp down-arrow"><span>→</span></a>
+            </div>
