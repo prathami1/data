13cfa834e068485f51f1e623ccf30d35b885f141
Pratham <62070812+prathami1@users.noreply.github.com>
2021-08-10 22:20:16 -0400

    configuring routes 🔗

---
+++ routes.config.js
@@ -0,0 +1,25 @@
+app.post('/users', [
+   UsersController.insert
+]);
+app.get('/users', [
+   ValidationMiddleware.validJWTNeeded,
+   PermissionMiddleware.minimumPermissionLevelRequired(PAID),
+   UsersController.list
+]);
+app.get('/users/:userId', [
+   ValidationMiddleware.validJWTNeeded,
+   PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+   PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+   UsersController.getById
+]);
+app.patch('/users/:userId', [
+   ValidationMiddleware.validJWTNeeded,
+   PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+   PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+   UsersController.patchById
+]);
+app.delete('/users/:userId', [
+   ValidationMiddleware.validJWTNeeded,
+   PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
+   UsersController.removeById
+]);
